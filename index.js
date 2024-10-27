45de1990091b7394c164307ff1ccf97172958e2d
Basirat Hamayoon <basirathumayun@gmail.com>
2024-10-27 09:40:45 +0500

    Add event listner function on all the colors

--- index.js
+++ index.js
@@ -9,3 +9,39 @@ var blackColor = document.getElementById('black');
 var brownColor = document.getElementById('brown');
 var defaultColor = document.getElementById('defaultColor');
 var mainHeading = document.getElementById('mainHeading');
+purpleColor.addEventListener('click', function(){
+   document.body.style.background = "purple";
+   mainHeading.style.color = "purple";
+});
+blueColor.addEventListener('click', function(){
+    document.body.style.background = "blue";
+    mainHeading.style.color = "blue";
+ });
+ greenColor.addEventListener('click', function(){
+    document.body.style.background = "green";
+    mainHeading.style.color = "green"; 
+ });
+ redColor.addEventListener('click', function(){
+    document.body.style.background = "red";
+    mainHeading.style.color = "red"; 
+ });
+ yellowColor.addEventListener('click', function(){
+    document.body.style.background = "yellow";
+    mainHeading.style.color = "yellow"; 
+ });
+ orangeColor.addEventListener('click', function(){
+    document.body.style.background = "orange";
+    mainHeading.style.color = "orange"; 
+ });
+ pinkColor.addEventListener('click', function(){
+    document.body.style.background = "pink";
+    mainHeading.style.color = "pink"; 
+ });
+ blackColor.addEventListener('click', function(){
+    document.body.style.background = "black";
+    mainHeading.style.color = "black"; 
+ });
+ brownColor.addEventListener('click', function(){
+    document.body.style.background = "brown";
+    mainHeading.style.color = "brown"; 
+ });
